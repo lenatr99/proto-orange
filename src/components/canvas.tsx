2d2d2d0e7d190fc270805462cc0d223ba233eac9
janezd <janez.demsar@fri.uni-lj.si>
2024-02-09 21:59:54 +0100

    Improve handling of mouse interactions

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -26,14 +26,14 @@ const ConnectingLine = ({x1, y1, x2, y2}) =>
     </g>
 
 
-const TempConnection = ({x1, y1, x2, y2, side}) => {
+const TempConnection = ({wx, wy, x, y, side}) => {
+    const x1 = wx + 1.25 * (side === "left" ? -widgetR : widgetR);
     return <g className="connection temp">
         { side === "right"
-            ? <Bezier className="connection"
-                      x1={x1} y1={y1} x2={x2} y2={y2} />
-            : <Bezier className="connection"
-                      x1={x2} y1={y2} x2={x1} y2={y1} /> }
-        <circle cx={x1} cy={y1} r={3} fill="grey"/>
+            ? <Bezier className="connection" x1={x1} y1={wy} x2={x} y2={y} />
+            : <Bezier className="connection" x1={x} y1={y} x2={x1} y2={wy} />
+        }
+        <circle cx={x1} cy={wy} r={3} fill="grey"/>
     </g>
 }
 
@@ -48,6 +48,8 @@ const Connection = ({x1, y1, x2, y2, onMouseUp}) => {
     </g>
 }
 
+
+
 interface IWidgetData {
     x: number;
     y: number;
@@ -58,6 +60,60 @@ interface IWidgetData {
 interface IWidgetList {
     [widgetId: string]: IWidgetData;
 }
+
+
+interface IMouseState {
+    state?: "selecting" | "connecting" | "moving";
+    x?: number;
+    y?: number;
+    downX?: number;
+    downY?: number;
+    distance: number;
+    moveOrigins: [string, number, number][];
+    sourceId?: string;
+    side?: "left" | "right"
+    targetId?: string;
+}
+
+interface ISelectionState extends IMouseState {
+    state: "selecting";
+    x: number;
+    y: number;
+    downX: number;
+    downY: number;
+}
+
+interface IConnectingState extends IMouseState {
+    state: "connecting";
+    x: number;
+    y: number;
+    sourceId: string;
+    side: "left" | "right";
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface IMovingState extends IMouseState {
+    state: "moving";
+    x: number;
+    y: number;
+    downX: number;
+    downY: number;
+    moveOrigins: [string, number, number][];
+}
+
+
+const initialMouseState: IMouseState = {
+    state: null, x: null, y: null, downX: null, downY: null, distance: 0, moveOrigins: [],
+    sourceId: null, side: null, targetId: null}
+
+
+const SelectionRect = ({downX, downY, x, y}) => {
+    const [srx1, srx2, sry1, sry2] = normalizedRect(downX, downY, x, y);
+    return <rect x={srx1} y={sry1}
+          width={srx2 - srx1} height={sry2 - sry1}
+          fill="lightblue" stroke="blue" fillOpacity="0.8" />
+}
+
 
 export const Canvas = () =>  {
     const newWidgetId = uuidv4;
@@ -143,21 +199,40 @@ export const Canvas = () =>  {
         socket, sessionId, "connection-action"
     );
 
-    const [{mouseX, mouseY}, setMouseState] = React.useState({mouseX: null, mouseY: null});
-    const [[downX, downY, distance], setDown] = React.useState([null, null, null]);
-    const [moveOrigins, setMoveState] = React.useState([]);
-    const [{sourceId, side, targetId }, setEarState] = React.useState({sourceId: null, side: null, targetId: null});
+    const [mouseState, setMouseState] =
+        React.useReducer((state: IMouseState, {type, ...args}) => {
+            const coords = args.x !== null? {x: args.x, y: args.y} : {};
+            switch (type) {
+                case "reset":
+                    return initialMouseState;
+                case "move":
+                    const [dx, dy] = [args.x - state.downX, args.y - state.downY];
+                    state.moveOrigins.forEach(([widgetId, ox, oy]) => {
+                        widgetAction({type: "dragWidget", widgetId, x: ox + dx, y: oy + dy});
+                    })
+                    return {...state, ...coords,
+                            distance: state.distance + (args.x - state.x) ** 2 + (args.y - state.y) ** 2};
+                case "setConnectionTarget":
+                    return {...state, targetId: args.targetId};
+
+                case "startMoving":
+                    return {...state, ...coords, state: "moving", moveOrigins: args.moveOrigins,
+                            downX: args.downX, downY: args.downY, x: args.downX, y: args.downY, distance: 0};
+                case "startSelecting":
+                    return {...state, ...coords, state: "selecting", downX: args.x, downY: args.y, distance: 0};
+                case "startConnecting":
+                    return {...state, ...coords, state: "connecting", sourceId: args.sourceId, side: args.side};
+                default:
+                    throw new Error(`Unknown action type: ${type}`);
+            }
+        },
+        initialMouseState);
+
     const [selection, setSelection] = React.useState([] as string[]);
 
     const addWidget = (x: number, y: number, defaultId=null) => {
         const widgetId = defaultId || newWidgetId();
         widgetAction({type: "addWidget", x, y, widgetId})
-    }
-
-    const resetMoveState = () => {
-        setDown([null, null, null]);
-        setMoveState([]);
-        setEarState({sourceId: null, side: null, targetId: null});
     }
 
     const clearSelection = () => {
@@ -166,94 +241,86 @@ export const Canvas = () =>  {
 
     const widgetMouse = (widgetId: string, x: number, y: number, event: React.MouseEvent) => {
         stopEvent(event);
-        const thisWidget = [widgetId, widgets[widgetId].x, widgets[widgetId].y];
+        const thisWidget: [string, number, number] = [widgetId, widgets[widgetId].x, widgets[widgetId].y];
+        let moveOrigins: [string, number, number][];
         if (!selection.includes(widgetId)) {
             clearSelection();
-            setMoveState([thisWidget]);
+            moveOrigins = [thisWidget];
         }
         else {
-            setMoveState([
-                thisWidget,
-                ...Array.from(
-                    selection.filter((id) => id !== widgetId),
-                    (id) => [id, widgets[id].x, widgets[id].y]
-                )
-            ]);
+            moveOrigins = [thisWidget,
+                ...Array.from(selection.filter((id) => id !== widgetId),
+                    (id) => [id, widgets[id].x, widgets[id].y] as [string, number, number])]
         }
-        setDown([x, y, 0]);
+        setMouseState({type: "startMoving", moveOrigins, downX: x, downY: y});
     }
 
     const earMouse = (widgetId: string, side: string, event: React.MouseEvent) => {
         stopEvent(event);
         clearSelection();
-        setEarState({sourceId: widgetId, side, targetId: null});
+        setMouseState({type: "startConnecting", sourceId: widgetId, x: event.clientX, y: event.clientY, side});
     }
 
     const onMouseDown = (event: React.MouseEvent) => {
-        setDown([event.clientX, event.clientY, 0]);
+        stopEvent(event);
+        setMouseState({type: "startSelecting", x: event.clientX, y: event.clientY});
     }
 
     const onMouseMove = (event: React.MouseEvent) => {
-        setMouseState({mouseX: event.clientX, mouseY: event.clientY});
-        setDown([downX, downY,
-            Math.max(distance,
-                     Math.sqrt((downX - event.clientX) ** 2
-                               + (downY - event.clientY) ** 2))
-        ]);
-        moveOrigins.forEach(([widgetId, ox, oy]) => {
-            widgetAction({type: "dragWidget", widgetId,
-                          x: ox + event.clientX - downX,
-                          y: oy + event.clientY - downY});
-        })
         stopEvent(event);
+        if (mouseState.state)
+            setMouseState({type: "move", x: event.clientX, y: event.clientY});
     }
 
     const onMouseUp = (event: React.MouseEvent) => {
-        if (moveOrigins.length) {
-            if (distance < 4) {
-                clearSelection();
-                widgetAction({type: "flipOpen", widgetId: moveOrigins[0][0]});
+        const notMoved = mouseState.distance < 16;
+        switch(mouseState.state) {
+            case "moving": {
+                if (notMoved) {
+                    clearSelection();
+                    widgetAction({type: "flipOpen", widgetId: mouseState.moveOrigins[0][0]});
+                }
+                else {
+                    const [dx, dy] = [event.clientX - mouseState.downX, event.clientY - mouseState.downY];
+                    mouseState.moveOrigins.forEach(([widgetId, ox, oy]) => {
+                        widgetAction({type: "moveWidget", widgetId, x: ox + dx, y: oy + dy});
+                    })
+                }
+                break;
             }
-            else {
-                moveOrigins.forEach(([widgetId, ox, oy]) => {
-                    widgetAction({type: "moveWidget", widgetId,
-                                  x: ox + event.clientX - downX,
-                                  y: oy + event.clientY - downY});
-                })
+            case "connecting": {
+                let {sourceId, targetId} = mouseState
+                if (!targetId) {
+                    targetId = newWidgetId();
+                    addWidget(event.clientX, event.clientY, targetId);
+                }
+                if (mouseState.side === "left") {
+                    [sourceId, targetId] = [targetId, sourceId];
+                }
+                if ((targetId !== sourceId)
+                    && !connections.find(([s, t]) => s === sourceId && t === targetId)) {
+                    connectionsAction({type: "addConnection", sourceId, targetId});
+                }
+                break;
             }
-        }
-        else if (sourceId) {
-            let actTargetId = targetId;
-            if (!actTargetId) {
-                actTargetId = newWidgetId();
-                addWidget(event.clientX, event.clientY, actTargetId);
-            }
-            if ((actTargetId !== sourceId) && (!connections.find(([s, t]) => s === sourceId && t === actTargetId))) {
-                if (side === "right") {
-                    connectionsAction({type: "addConnection",
-                                       sourceId, targetId: actTargetId});
-                } else {
-                    connectionsAction({type: "addConnection",
-                                       sourceId: actTargetId, targetId: sourceId});
+            case "selecting": {
+                if (notMoved) {
+                    clearSelection();
+                    addWidget(event.clientX, event.clientY);
+                }
+                else {
+                    const [x1, x2, y1, y2] = normalizedRect(mouseState.downX, mouseState.downY,
+                                                            event.clientX, event.clientY);
+                    setSelection(Object.keys(widgets).filter((widgetId) => {
+                        const {x, y} = widgets[widgetId];
+                        return x + widgetR > x1 && x - widgetR < x2
+                            && y + widgetR > y1 && y - widgetR < y2;
+                    }))
                 }
             }
         }
-        else if (downX !== null && distance > 4) {
-            if (!moveOrigins.length) {
-                const [x1, x2, y1, y2] = normalizedRect(downX, downY,
-                                                        event.clientX, event.clientY);
-                setSelection(Object.keys(widgets).filter((widgetId) => {
-                    const widget = widgets[widgetId];
-                    return widget.x + widgetR > x1 && widget.x - widgetR < x2
-                        && widget.y + widgetR > y1 && widget.y - widgetR < y2;
-                }))
-            }
-        }
-        else {
-            clearSelection();
-            addWidget(event.clientX, event.clientY);
-        }
-        resetMoveState();
+        setMouseState({type: "reset"});
+        stopEvent(event);
     }
 
     const keyHandler = (event: React.KeyboardEvent) => {
@@ -263,19 +330,14 @@ export const Canvas = () =>  {
             clearSelection();
         }
     }
-    const registerHover = (widgetId: string, hover) => {
-        if (sourceId) {
-            if (hover) {
-                setEarState(state => ({...state, targetId: widgetId}))
-            } else if (targetId === widgetId) {
-                setEarState(state => ({...state, targetId: null}))
-            }
+    const registerHover = (widgetId: string, hover: boolean) => {
+        if (mouseState.state === "connecting"
+            && (hover || mouseState.targetId === widgetId)) {
+                setMouseState({type: "setConnectionTarget", targetId: hover ? widgetId : null})
         }
     }
 
-    const [srx1, srx2, sry1, sry2] = normalizedRect(downX, downY, mouseX, mouseY);
-
-    return <svg width="100%" height="1000" tabIndex={0}
+    return <svg width="100%" height="1000" tabIndex={0} ref={(el) => el && el.focus()}
                 onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp}
                 onKeyUp={keyHandler}>
         { connections.map(([sourceId, targetId]) => {
@@ -290,17 +352,14 @@ export const Canvas = () =>  {
                                    {type: "removeConnection", sourceId, targetId})}
             />})
         }
-        { sourceId
-            && <TempConnection x1={widgets[sourceId].x
-                                   + 1.25 * (side === "left" ? -widgetR : widgetR)}
-                               y1={widgets[sourceId].y}
-                               x2={mouseX}
-                               y2={mouseY}
-                               side={side} /> }
-        { downX && !sourceId && !moveOrigins.length &&
-            <rect x={srx1} y={sry1}
-                  width={srx2 - srx1} height={sry2 - sry1}
-                  fill="lightblue" stroke="blue" fillOpacity="0.8" /> }
+        { mouseState.state === "connecting"
+            && TempConnection({wx: widgets[mouseState.sourceId].x,
+                               wy: widgets[mouseState.sourceId].y,
+                               ...mouseState as IConnectingState})
+        }
+        { mouseState.state === "selecting"
+            && SelectionRect(mouseState as ISelectionState)
+        }
         { Object.values(widgets).map((widget) =>
             <Widget key={widget.widgetId} data={widget}
                     onMouseWidget={widgetMouse} onMouseEar={earMouse}
